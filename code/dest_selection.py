a8887a5c437c13e306847cb4d11140cb32f8afab
galaxies99 <galaxies@vip.qq.com>
2020-06-06 21:09:27 +0800

    Update

--- code/dest_selection.py
+++ code/dest_selection.py
@@ -1,5 +1,4 @@
 import csvreader
-import numpy as np
 import matplotlib.pyplot as plt
 import time
 
@@ -19,10 +18,8 @@ def datetime_timestamp(dt):
 
 if __name__ == '__main__':
     dest_cnt = {}
-    dat1 = csvreader.csv_reader_no_headers("D:/Algorithm and Complexity Group Project/order_20161105")
-    dat2 = csvreader.csv_reader_no_headers("D:/Algorithm and Complexity Group Project/order_20161106")
-    dat3 = csvreader.csv_reader_no_headers("D:/Algorithm and Complexity Group Project/order_20161107")
-    dat = dat1 + dat2 + dat3
+    dat = csvreader.csv_reader_no_headers("../../CS214-CourseData/Projects/data/chengdu_order/order_20161105")
+    
     for line in dat:
         dest_longitude = float(line[5])
         dest_latitude = float(line[6])
@@ -34,8 +31,6 @@ if __name__ == '__main__':
         s = s[11:]
         s1 = '23:00:00'
         s2 = '06:00:00'
-        # d1 = datetime_timestamp('2016-11-05 23:00:00')
-        # d2 = datetime_timestamp('2016-11-05 06:00:00')
         if s >= s1 or s <= s2:
             index = str(dest_brick_lon) + '.' + str(dest_brick_lat)
             if dest_cnt.get(index) is None:
@@ -60,7 +55,7 @@ if __name__ == '__main__':
 
         print('longitude =', item[1] / 500.0, 'latitude =', item[2] / 500.0, 'count =', item[0])
 
-    print(len(dest_lon))
+    print("total =" len(dest_lon))
 
-    plt.plot(dest_lon, dest_lat, 'o', markersize=2.)
+    plt.plot(dest_lon, dest_lat, 'o', markersize=2)
     plt.show()
