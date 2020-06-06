217d2a07faf5ec416fb7c712310113814928e3fc
P.S Yan <1050335889@sjtu.edu.cn>
2020-06-06 18:28:11 +0800

    Update departure selection
    add timestamp comparison

---
+++ code/departure_selection.py
@@ -0,0 +1,71 @@
+import csvreader
+import numpy as np
+import matplotlib.pyplot as plt
+from pyecharts.charts import HeatMap
+from pyecharts import options as opts
+
+if __name__ == '__main__':
+    dest_cnt = {}
+    dat = csvreader.csv_reader_no_headers("../../CS214-CourseData/Projects/data/chengdu_order/order_20161105")
+    for line in dat:
+        dest_longitude = float(line[3])
+        dest_latitude = float(line[4])
+
+        dest_brick_lon = int(dest_longitude * 50)
+        dest_brick_lat = int(dest_latitude * 50)
+
+        index = str(dest_brick_lon) + '.' + str(dest_brick_lat)
+        if dest_cnt.get(index) is None:
+            dest_cnt[index] = 1
+        else:
+            dest_cnt[index] += 1
+
+    dest = []
+    for key in dest_cnt.keys():
+        dest_brick_lon, dest_brick_lat = key.split('.')
+        dest.append([dest_cnt[key], int(dest_brick_lon), int(dest_brick_lat)])
+
+    dest.sort()
+
+    for item in dest:
+        print('longitude =', item[1] / 50.0, 'latitude =', item[2] / 50.0, 'count =', item[0])
+
+    x_axis = []
+    y_axis = []
+    for i in range(10380, 10430, 2):
+        x_axis.append(i / 100.0)
+    for j in range(3040, 3100, 2):
+        y_axis.append(j / 100.0)
+
+    data = [[i, j, 0] for i in range(len(x_axis)) for j in range(len(y_axis))]
+
+    for item in dest:
+        if item[0] < 5:
+            continue
+        ix = int((item[1] / 50.0 - 103.8) / 0.02)
+        iy = int((item[2] / 50.0 - 30.4) / 0.02)
+        for d_item in data:
+            if d_item[0] == ix and d_item[1] == iy:
+                d_item[2] = item[0]
+                break
+
+    Max = 0
+    for item in data:
+        Max = max(Max, item[2])
+
+    for item in data:
+        item[2] = int(item[2] / Max * 100)
+
+    heat_map = (
+        HeatMap()
+        .add_xaxis(x_axis)
+        .add_yaxis("HeatMap",
+                   y_axis,
+                   data,
+                   label_opts=opts.LabelOpts(is_show=True, position="inside"),)
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title="HeatMap"),
+            visualmap_opts=opts.VisualMapOpts(),
+        )
+        .render("heatmap.html")
+    )
--- code/dest_selection.py
+++ code/dest_selection.py
@@ -1,4 +1,5 @@
 import csvreader
+import numpy as np
 import matplotlib.pyplot as plt
 import time
 
@@ -22,13 +23,23 @@ def datetime_timestamp(dt):
 
 if __name__ == '__main__':
     dest_cnt = {}
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+>>>>>>> Stashed changes
     dat1 = csvreader.csv_reader_no_headers("D:\Algorithm and Complexity Group Project\order_20161105")
     dat2 = csvreader.csv_reader_no_headers("D:\Algorithm and Complexity Group Project\order_20161106")
     dat3 = csvreader.csv_reader_no_headers("D:\Algorithm and Complexity Group Project\order_20161107")
     dat = dat1 + dat2 + dat3
+<<<<<<< Updated upstream
+=======
+=======
+    dat = csvreader.csv_reader_no_headers("../../CS214-CourseData/Projects/data/chengdu_order/order_20161105")
+>>>>>>> d81641c0e5cc6207abdcf6a008d2872513b37669
+>>>>>>> Stashed changes
     for line in dat:
-        dest_longitude = float(line[3])
-        dest_latitude = float(line[4])
+        dest_longitude = float(line[5])
+        dest_latitude = float(line[6])
 
         dest_brick_lon = int(dest_longitude * 500)
         dest_brick_lat = int(dest_latitude * 500)
@@ -57,7 +68,7 @@ if __name__ == '__main__':
     dest_lat = []
 
     for item in dest:
-        if item[0] > 100:
+        if item[0] > 500:
             dest_lon.append(item[1] / 500.0)
             dest_lat.append(item[2] / 500.0)
 
