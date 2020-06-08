ab294c1c458594b7526c57af156f932bd28410e4
galaxies99 <galaxies@vip.qq.com>
2020-06-09 00:16:14 +0800

    Fix some bugs.

--- code/tsp.py
+++ code/tsp.py
@@ -14,9 +14,9 @@ def tsp(n, cities):
             dist[i][j] = math.sqrt((cities[i][0] - cities[j][0]) ** 2 + (cities[i][1] - cities[j][1]) ** 2)
 
     if n <= D:
-        return TSP_dp.TSP_solver(n, dist)
+        return tsp_dp.TSP_solver(n, dist)
     else:
-        return TSP_gene.TSP_solver(n, dist)
+        return tsp_gene.TSP_solver(n, dist)
 
 
 def tsp_lat_lng(n, lat, lng):
@@ -27,9 +27,9 @@ def tsp_lat_lng(n, lat, lng):
             dist[i][j] = math.sqrt((lat[i] - lat[j]) ** 2 + (lng[i] - lng[j]) ** 2)
 
     if n <= D:
-        return TSP_dp.TSP_solver(n, dist)
+        return tsp_dp.TSP_solver(n, dist)
     else:
-        return TSP_gene.TSP_solver(n, dist)
+        return tsp_gene.TSP_solver(n, dist)
 
 
 if __name__ == '__main__':
